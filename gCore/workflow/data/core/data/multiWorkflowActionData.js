80719d9a9a7d07f38abd221b98a1f98003a1a971
Nodics <nodics.framework@gmail.com>
2020-03-19 19:45:01 +0400

    Tested multi channel qualifier process

---
+++ gCore/workflow/data/core/data/multiWorkflowActionData.js
@@ -0,0 +1,58 @@
+/*
+    Nodics - Enterprice Micro-Services Management Framework
+
+    Copyright (c) 2017 Nodics All rights reserved.
+
+    This software is the confidential and proprietary information of Nodics ("Confidential Information").
+    You shall not disclose such Confidential Information and shall use it only in accordance with the 
+    terms of the license agreement you entered into with Nodics.
+
+ */
+
+module.exports = {
+    record0: {
+        code: "multiActionSuccess",
+        name: "multiActionSuccess",
+        active: true,
+        type: ENUMS.WorkflowActionType.AUTO.key,
+        position: ENUMS.WorkflowActionPosition.END.key,
+        handler: 'DefaultSampleWorkflowService.performActionThree',
+        userGroups: ['adminUserGroup']
+    },
+    record1: {
+        code: "multiActionOne",
+        name: "multiActionOne",
+        active: true,
+        type: ENUMS.WorkflowActionType.MANUAL.key,
+        userGroups: ['adminUserGroup'],
+        allowedDecisions: ['SUCCESS'],
+        channels: ['multiSuccessChannel']
+    },
+    record2: {
+        code: "multiActionTwo",
+        name: "multiActionTwo",
+        active: true,
+        type: ENUMS.WorkflowActionType.MANUAL.key,
+        userGroups: ['adminUserGroup'],
+        allowedDecisions: ['SPLIT'],
+        channels: ['multiSplitOneChannel', 'multiSplitTowChannel']
+    },
+    record3: {
+        code: "multiSplitActionOne",
+        name: "multiSplitActionOne",
+        active: true,
+        type: ENUMS.WorkflowActionType.MANUAL.key,
+        userGroups: ['adminUserGroup'],
+        allowedDecisions: ['SUCCESS'],
+        channels: ['multiSuccessChannel']
+    },
+    record4: {
+        code: "multiSplitActionTwo",
+        name: "multiSplitActionTwo",
+        active: true,
+        type: ENUMS.WorkflowActionType.MANUAL.key,
+        userGroups: ['adminUserGroup'],
+        allowedDecisions: ['SUCCESS', 'MultiSplitReject'],
+        channels: ['multiSuccessChannel', 'multiSplitTowRejectChannel']
+    }
+};
---
+++ gCore/workflow/data/core/data/multiWorkflowChannelData.js
@@ -0,0 +1,68 @@
+/*
+    Nodics - Enterprice Micro-Services Management Framework
+
+    Copyright (c) 2017 Nodics All rights reserved.
+
+    This software is the confidential and proprietary information of Nodics ("Confidential Information").
+    You shall not disclose such Confidential Information and shall use it only in accordance with the 
+    terms of the license agreement you entered into with Nodics.
+
+ */
+
+module.exports = {
+
+    record0: {
+        code: 'multiSuccessChannel',
+        name: 'multiSuccessChannel',
+        active: true,
+        qualifier: {
+            decision: 'SUCCESS'
+        },
+        target: 'multiActionSuccess'
+    },
+    record1: {
+        code: "multiOneChannel",
+        name: "multiOneChannel",
+        active: true,
+        qualifier: {
+            decision: 'MultiOne'
+        },
+        target: 'multiActionOne'
+    },
+    record2: {
+        code: "multiTwoChannel",
+        name: "multiTwoChannel",
+        active: true,
+        qualifier: {
+            decision: 'MultiTwo'
+        },
+        target: 'multiActionTwo'
+    },
+    record3: {
+        code: "multiSplitOneChannel",
+        name: "multiSplitOneChannel",
+        active: true,
+        qualifier: {
+            decision: 'SPLIT'
+        },
+        target: 'multiSplitActionOne'
+    },
+    record4: {
+        code: "multiSplitTowChannel",
+        name: "multiSplitTowChannel",
+        active: true,
+        qualifier: {
+            decision: 'SPLIT'
+        },
+        target: 'multiSplitActionTwo'
+    },
+    record5: {
+        code: "multiSplitTowRejectChannel",
+        name: "multiSplitTowRejectChannel",
+        active: true,
+        qualifier: {
+            decision: 'MultiSplitReject'
+        },
+        target: 'multiActionTwo'
+    }
+};
